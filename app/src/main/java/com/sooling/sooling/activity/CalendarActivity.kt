e7f40f614950ad35958debcf8d891b940e829a76
yunji <zion830@naver.com>
2018-12-31 02:51:54 +0900

    Add main activity layout

---
+++ app/src/main/java/com/sooling/sooling/activity/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.sooling.sooling.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CalendarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/sooling/sooling/activity/MainActivity.kt
+++ app/src/main/java/com/sooling/sooling/activity/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.sooling.sooling.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.sooling.sooling.R
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        Glide.with(applicationContext)
+                .load(R.drawable.test)
+                .apply(RequestOptions().circleCrop())
+                .into(iv_main_profile)
+
+        btn_main_capacity.setOnClickListener(this)
+        btn_main_calendar.setOnClickListener(this)
+        btn_main_wiki.setOnClickListener(this)
+        btn_main_setting.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_main_capacity -> startActivity<AddCapacityActivity>()
+            R.id.btn_main_calendar -> startActivity<CalendarActivity>()
+            R.id.btn_main_wiki -> startActivity<WikiActivity>()
+            R.id.btn_main_setting -> startActivity<SettingActivity>()
+        }
     }
 }
